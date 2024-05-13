bf88f843201bf1fba47711eed2fb3c3571594e81
jeoung ju <hjj97753967@gmail.com>
2024-05-13 16:52:02 +0900

    home_menu

--- app/src/main/java/com/example/rootmap/MainActivity.kt
+++ app/src/main/java/com/example/rootmap/MainActivity.kt
@@ -4,20 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rootmap.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
-
+     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+       // binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
 
         //binding.emailTv.text = auth.currentUser?.email
+
+
+        val contextList= listOf(MenuFragment(),MenuFragment2(),MenuFragment3(),MenuFragment4())
+        val adapter=HomeFragmentAdapter(this)
+        adapter.fragmentList=contextList
+        binding.viewPager.adapter=adapter
+
+        //하단에 탭 바 구성, 클릭 시 해당 프레그먼트로 이동
+        val tabTitle= listOf<String>("메뉴1","메뉴2","메뉴3","기타")
+        TabLayoutMediator(binding.tabLayout,binding.viewPager){ tab,position->
+            tab.text=tabTitle[position]
+        }.attach()
+
 
         binding.logoutBtn.setOnClickListener {
             auth.signOut() // 로그아웃 처리
@@ -25,5 +39,7 @@ class MainActivity : AppCompatActivity() {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent) // LoginActivity로 화면 전환
         }
+       // */
+
     }
 }
