644ec336c0ecddb87b742a3ab7133382625f0e78
jeoung ju <hjj97753967@gmail.com>
2024-05-27 18:53:31 +0900

    지도가 현위치로

--- app/src/main/java/com/example/rootmap/MenuFragment3.kt
+++ app/src/main/java/com/example/rootmap/MenuFragment3.kt
@@ -1,11 +1,18 @@
 package com.example.rootmap
 
+import android.Manifest
+import android.location.GnssAntennaInfo.Listener
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.lifecycleScope
 import com.example.rootmap.databinding.FragmentMenu2Binding
 import com.example.rootmap.databinding.FragmentMenu3Binding
 import com.example.rootmap.databinding.FragmentMenuBinding
@@ -14,7 +21,10 @@ import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
+import kotlinx.coroutines.async
 import java.lang.Exception
+import javax.security.auth.callback.Callback
+import kotlin.coroutines.suspendCoroutine
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,7 +39,10 @@ private const val ARG_PARAM2 = "param2"
 class MenuFragment3 : Fragment() {
     private lateinit var mapview: MapView
     private var zoomlevel = 13
-    private var startpositon = LatLng.from(37.394660,127.111182)
+  // private var startpositon = LatLng.from(37.394660,127.111182)
+    lateinit var locationPermission: ActivityResultLauncher<String>
+    private lateinit var locationService: LocationService
+    var startpositon:LatLng? = null
 
     //프래그먼트의 binding
     val binding by lazy { FragmentMenu3Binding.inflate(layoutInflater) }
@@ -37,7 +50,7 @@ class MenuFragment3 : Fragment() {
     private val readyCallback = object: KakaoMapReadyCallback(){
         override fun onMapReady(p0: KakaoMap) {}
         override fun getPosition(): LatLng {
-            return startpositon
+            return startpositon!!
         }
 
         override fun getZoomLevel(): Int {
@@ -61,7 +74,6 @@ class MenuFragment3 : Fragment() {
             Toast.makeText(context,"map error!", Toast.LENGTH_SHORT).show()
         }
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -74,10 +86,23 @@ class MenuFragment3 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
       //  binding= FragmentMenu3Binding.inflate(inflater, container, false)
-
+        locationService = LocationService(requireContext())
         //여기부터 코드 작성
+        locationPermission=registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted->
+            if (isGranted){
+                fetchLocation()
+            }else{
+                Toast.makeText(context,"위치 권한을 승인하여야 사용할 수 있습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+        locationPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         mapview = binding.mapView.findViewById(R.id.map_view)
-        mapview.start(lifecycleCallback, readyCallback)
+        viewLifecycleOwner.lifecycleScope.async{
+            fetchLocation()
+            if(startpositon!=null){
+
+            }
+        }
 
         binding.addButton.setOnClickListener {
             Toast.makeText(context,"클릭",Toast.LENGTH_SHORT).show()
@@ -87,6 +112,14 @@ class MenuFragment3 : Fragment() {
         }
         //
         return binding.root
+    }
+    private fun fetchLocation(){
+        locationService.fetchLocation { latitude, longitude ->
+            startpositon=LatLng.from(latitude,longitude)
+            Log.d("test","${latitude}, ${longitude}")
+            binding.progressBar.visibility=View.GONE
+            mapview.start(lifecycleCallback, readyCallback)
+        }
     }
 
     companion object {
